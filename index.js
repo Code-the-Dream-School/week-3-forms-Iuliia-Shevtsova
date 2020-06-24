c125d6389a7b185cf5ef2dc0bea700cff5daf4ad
Iuliia Shevtsova <uliyaal2018@gmail.com>
2020-06-24 01:42:13 -0400

    created two forms using bootstrap

---
+++ index.js
@@ -0,0 +1,9 @@
+$(function(){
+    $('#date').daterangepicker({
+    singleDatePicker: true,
+    showDropdowns: true,
+    locale: 'es-es' ,
+    format: 'DD.MM.YYYY',
+    language: 'en',
+    });
+});
